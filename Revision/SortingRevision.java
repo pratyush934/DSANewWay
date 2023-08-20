5582b53b566279d23c05ce35515f2693802f1a43
Pratyush <oggycandle786@gmail.com>
2023-08-20 17:29:42 +0530

    Revision1

--- Revision/SortingRevision.java
+++ Revision/SortingRevision.java
@@ -1,7 +1,7 @@
 public class SortingRevision {
 
     public static int[] bubbleSort(int arr[]) {
-        for (int turn = 0; turn < arr.length - 1; turn++) {
+        for (int turn = 0; turn < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - turn - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
@@ -29,97 +29,100 @@ public class SortingRevision {
     }
 
     public static int[] insertionSort(int arr[]) {
-
         for (int i = 1; i < arr.length; i++) {
             int curr = arr[i];
             int prev = i - 1;
-            while(prev >=0 && curr < arr[prev]) {
-                arr[prev+1] = arr[prev];
+
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
                 prev--;
             }
-            arr[prev+1] = curr;
+            arr[prev + 1] = curr;
         }
         return arr;
     }
 
     public static int[] countingSort(int arr[]) {
-        int maxNum = Integer.MIN_VALUE;
-        for(int i=0; i<arr.length; i++) {
-            maxNum = Math.max(maxNum, arr[i]);
+        int maxNumber = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (maxNumber < arr[i]) {
+                maxNumber = arr[i];
+            }
         }
-        int count[] = new int[maxNum+1];
-        for(int i=0; i<arr.length; i++) {
+        int count[] = new int[maxNumber + 1];
+        for (int i = 0; i < arr.length; i++) {
             count[arr[i]]++;
         }
-        int j=0;
-        for(int i=0; i<count.length; i++) {
-            while(count[i] > 0) {
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
                 arr[j] = i;
+                j--;
                 count[i]--;
-                j++;
             }
         }
         return arr;
     }
 
     public static void mergeSort(int arr[], int si, int ei) {
-        if(si >= ei) {
+        if (si >= ei) {
             return;
         }
-
         int mid = si + (ei - si) / 2;
         mergeSort(arr, si, mid);
-        mergeSort(arr, mid+1, ei);
+        mergeSort(arr, mid + 1, ei);
         merge(arr, si, ei, mid);
     }
 
     private static void merge(int[] arr, int si, int ei, int mid) {
         int temp[] = new int[arr.length];
         int i = si;
-        int j = mid+1;
+        int j = mid + 1;
         int k = 0;
 
-        while(i <= mid && j <= ei) {
-            if(arr[i] < arr[j]) {
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
                 temp[k++] = arr[i++];
             } else {
                 temp[k++] = arr[j++];
             }
         }
-        while(i <= mid) {
+        while (i <= mid) {
             temp[k++] = arr[i++];
         }
-        while(j <= ei) {
+        while (j <= ei) {
             temp[k++] = arr[j++];
         }
-        for(k=0, i=si; k<temp.length; k++, i++) {
+
+        for (k = 0, i = si; k < temp.length; k++, i++) {
             arr[i] = temp[k];
         }
     }
 
     public static void quickSort(int arr[], int si, int ei) {
-        if(si >= ei) {
+        if (si >= ei) {
             return;
         }
         int pIdx = partition(arr, si, ei);
-        quickSort(arr, si, pIdx-1);
-        quickSort(arr, pIdx+1, ei);
+        quickSort(arr, si, pIdx - 1);
+        quickSort(arr, pIdx + 1, ei);
+
     }
 
     private static int partition(int[] arr, int si, int ei) {
         int pivot = arr[ei];
-        int i = -1;
+        int i = si - 1;
 
-        for(int j=si; j<arr.length; j++) {
-            if(arr[j] < pivot) {
+        for (int k = si; k <= ei; k++) {
+            if (arr[k] <= pivot) {
                 i++;
                 /* swap */
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                int temp = arr[k];
+                arr[k] = arr[i];
+                arr[i] = temp;
             }
         }
-        i++; /* ? */
+        i++;
         int temp = pivot;
         arr[ei] = arr[i];
         arr[i] = temp;
