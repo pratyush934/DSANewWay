ace8343344e6b712b37113996bd8002765dcfd2d
Pratyush <oggycandle786@gmail.com>
2023-08-24 11:59:52 +0530

    Revision

--- Revision/BinaryTreeTutRevision.java
+++ Revision/BinaryTreeTutRevision.java
@@ -1,6 +1,8 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
+import BinaryTreeTut.Information;
+
 public class BinaryTreeTutRevision {
 
     static class Node {
@@ -21,7 +23,7 @@ public class BinaryTreeTutRevision {
 
         public static Node buildTree(int nodes[]) {
             idx++;
-            if(nodes[idx] == -1) {
+            if (nodes[idx] == -1) {
                 return null;
             }
             Node newNode = new Node(nodes[idx]);
@@ -34,61 +36,126 @@ public class BinaryTreeTutRevision {
     }
 
     public static void preOrder(Node root) {
-        if(root == null) {
-            System.out.print(-1+" ");
+        if (root == null) {
+            System.out.print(-1 + " ");
             return;
         }
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
         preOrder(root.left);
         preOrder(root.right);
     }
 
     public static void inOrder(Node root) {
-        if(root == null) {
-            System.out.print(-1+" ");
+        if (root == null) {
+            System.out.print(-1 + " ");
             return;
         }
         inOrder(root.left);
-        System.out.println(root.data+" ");
+        System.out.println(root.data + " ");
         inOrder(root.right);
     }
 
     public static void postOrder(Node root) {
-        if(root == null) {
-            System.out.print(-1+" ");
+        if (root == null) {
+            System.out.print(-1 + " ");
         }
         postOrder(root.left);
         postOrder(root.right);
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
     }
 
     public static void levelOrder(Node root) {
-        if(root == null) {
+        if (root == null) {
             return;
         }
         Queue<Node> q = new LinkedList<>();
         q.add(root);
         q.add(null);
 
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             Node curr = q.remove();
-            if(curr == null) {
+            if (curr == null) {
                 System.out.println();
-                if(q.isEmpty()) {
+                if (q.isEmpty()) {
                     break;
                 } else {
                     q.add(null);
                 }
             } else {
-                System.out.print(curr.data+" ");
-                if(curr.left != null) {
-                    
+                System.out.print(curr.data + " ");
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
                 }
             }
         }
     }
 
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int lh = height(root.left);
+        int rh = height(root.right);
+        return Math.max(lh, rh) + 1;
+    }
+
+    public static int countNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int lCount = countNodes(root.left);
+        int rCount = countNodes(root.right);
+        return lCount + rCount + 1;
+    }
+
+    public static int sumOfNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int lSum = sumOfNodes(root.left);
+        int rSum = sumOfNodes(root.right);
+        return lSum + rSum + root.data;
+    }
+
+    public static int diameter(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int lh = height(root.left);
+        int rh = height(root.right);
+        int lDiam = diameter(root.left);
+        int rDiam = diameter(root.right);
+
+        return Math.min(lh + rh + 1, Math.max(lDiam, rDiam));
+    }
+
+    static class InfoDiameter {
+        int ht;
+        int diam;
+
+        public InfoDiameter(int ht, int diam) {
+            this.ht = ht;
+            this.diam = diam;
+        }
+    }
+
+    public static InfoDiameter diameterInfo(Node root) {
+        if(root == null) {
+            new InfoDiameter(0, 0);
+        }
+        InfoDiameter lDiam = diameterInfo(root.left);
+        InfoDiameter rDiam = diameterInfo(root.right);
+
+        int diam = Math.max(lDiam.ht+rDiam.ht+1, Math.max(lDiam.diam, rDiam.diam));
+        int ht = Math.max(lDiam.ht, rDiam.ht) + 1;
+
+        return new InfoDiameter(ht, diam);
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
