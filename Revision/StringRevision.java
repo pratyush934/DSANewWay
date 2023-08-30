41e026b264c268472db01cfef6f69087c2d3cde9
Pratyush <oggycandle786@gmail.com>
2023-08-31 00:42:18 +0530

    Revision 30th Aug

--- Revision/StringRevision.java
+++ Revision/StringRevision.java
@@ -3,9 +3,8 @@ public class StringRevision {
     public static boolean isPalindrome(String str) {
         int start = 0, end = str.length() - 1;
         while (start <= end) {
-            if (str.charAt(start) != str.charAt(end)) {
+            if (str.charAt(start) != str.charAt(end))
                 return false;
-            }
             start++;
             end--;
         }
@@ -15,9 +14,8 @@ public class StringRevision {
     public static boolean isPalindrome1(String str) {
         int n = str.length();
         for (int i = 0; i < n / 2; i++) {
-            if (str.charAt(i) != str.charAt(n - i - 1)) {
+            if (str.charAt(i) != str.charAt(n - i - 1))
                 return false;
-            }
         }
         return true;
     }
@@ -26,23 +24,23 @@ public class StringRevision {
         int x = 0, y = 0;
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-            if (ch == 'N')
-                y++;
-            else if (ch == 'S')
-                y--;
-            else if (ch == 'E')
+            if (ch == 'E')
                 x++;
-            else
+            else if (ch == 'W')
                 x--;
+            else if (ch == 'N')
+                y++;
+            else
+                y--;
         }
-        return (float) (Math.sqrt(x * x + y * y));
+        return (float)(Math.sqrt(x*x+y*y));
     }
 
     public static String upperCaseString(String str) {
         StringBuffer sb = new StringBuffer();
         sb.append(Character.toUpperCase(str.charAt(0)));
-        for (int i = 1; i < str.length(); i++) {
-            if (str.charAt(i) == ' ' && i < str.length() - 1) {
+        for(int i=1; i<str.length(); i++) {
+            if(str.charAt(i) == ' ' && i < str.length()-1) {
                 sb.append(str.charAt(i));
                 i++;
                 sb.append(Character.toUpperCase(str.charAt(i)));
@@ -58,7 +56,7 @@ public class StringRevision {
         Integer count = 1;
         for(int i=0; i<str.length(); i++) {
             count = 1;
-            while(i < str.length()-1 && str.charAt(i) == str.charAt(i+1) ) {
+            while(i < str.length()-1 && str.charAt(i) == str.charAt(i+1)) {
                 i++;
                 count++;
             }
@@ -69,8 +67,9 @@ public class StringRevision {
         }
         return sb.toString();
     }
+    
 
     public static void main(String[] args) {
-        System.out.println(compressString("aaannccc"));
+        System.out.println(compressString("aaabbbcccddde"));
     }
 }
