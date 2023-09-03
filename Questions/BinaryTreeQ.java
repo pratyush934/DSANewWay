7d4b2e85a4431938a09d1541f290661a67f4439a
Pratyush <oggycandle786@gmail.com>
2023-09-03 22:23:23 +0530

    Adding Questions

---
+++ Questions/BinaryTreeQ.java
@@ -0,0 +1,54 @@
+package Questions;
+
+import java.util.ArrayList;
+
+public class BinaryTreeQ {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static ArrayList<Integer> preOrder(Node root) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        if(root != null) {
+            list.add(root.data);
+            list.addAll(preOrder(root.left));
+            list.addAll(preOrder(root.right));
+        }
+        return list;
+    }
+
+    public static ArrayList<Integer> inOrder(Node root) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        if(root != null) {
+            list.addAll(inOrder(root.left));
+            list.add(root.data);
+            list.addAll(inOrder(root.right));
+        }
+        return list;
+    }
+
+    public static ArrayList<Integer> postOrder(Node root) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        if(root != null) {
+            list.addAll(postOrder(root.left));
+            list.addAll(postOrder(root.right));
+            list.add(root.data);
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Questions/Mathematical.java
@@ -0,0 +1,15 @@
+package Questions;
+
+public class Mathematical {
+
+    public static long summedMatrix(long n, long q) {
+        if (2 * n < q)
+            return 0;
+        else
+            return (long) (n - Math.abs((n + 1) - q));
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
