75410ef8e371a89768a3b8e9bbf3b98be87f280c
Pratyush <oggycandle786@gmail.com>
2023-09-25 22:03:50 +0530

    A

--- Questions/Mathematical.java
+++ Questions/Mathematical.java
@@ -11,6 +11,10 @@ public class Mathematical {
             return (long) (n - Math.abs((n + 1) - q));
     }
 
+    public static int test1() {
+        return 0;
+    }
+
     public static void main(String[] args) {
 
     }
