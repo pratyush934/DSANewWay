f312c3981e99f2340c9591d979d3792a5a4ccbc0
Pratyush <oggycandle786@gmail.com>
2023-11-17 18:29:27 +0530

    Adding dijkstra

--- Revision/Graphs.java
+++ Revision/Graphs.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -329,6 +330,58 @@ public class Graphs {
         }
     }
 
+    static class Pair implements Comparable<Pair>{
+        int n;
+        int path;
+
+        public Pair(int n, int path) {
+            this.n = n;
+            this.path = path;
+        }
+
+        @Override
+        public int compareTo(Graphs.Pair p2) {
+            return this.path - p2.path;
+        }
+    }
+
+
+    public static void dijkstraAlgo(ArrayList<Edge>[] graph, int src) {
+        int dist[] = new int[graph.length];
+        for(int i=0; i<graph.length; i++) {
+            if(i != src) {
+                dist[i] = Integer.MAX_VALUE; //+infinity
+            }
+        }
+
+        boolean vis[] = new boolean[graph.length]
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(src, 0));
+
+        //loop
+        while(!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            if(!vis[curr.n]) {
+                vis[curr.n] = true;
+                for(int i=0; i<graph[curr.n].size(); i++) {
+                    Edge e = graph[curr.n].get(i);
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+
+                    if(dist[u] + wt < dist[v] ) {
+                        dist[v] = dist[u] + wt;
+                        pq.add(new Pair(v, dist[v]));
+                    }
+                }
+            }
+        }
+        //print all source to vertices shortest dist;
+        for(int i=0; i<dist.length; i++) {
+            System.out.print(dist[i]+"-->");
+        }
+        System.out.println();
+    } 
     public static void main(String[] args) {
 
         createGraph();
