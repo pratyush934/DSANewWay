dbd85c0175b13b2381e2523df3bd040ca13f01d2
Pratyush <oggycandle786@gmail.com>
2023-11-18 17:16:00 +0530

    Adding Cheapest flight

--- Revision/Graphs.java
+++ Revision/Graphs.java
@@ -453,6 +453,72 @@ public class Graphs {
         }
         System.out.println("Final cost of MST : " + finalCost);
     }
+
+
+    public static void createGraph(int flights[][], ArrayList<Edge> graph[]) {
+        for(int i=0; i<graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i=0; i<flights.length; i++) {
+            int src = flights[i][0];
+            int dest = flights[i][1];
+            int wt = flights[i][2];
+
+            Edge e = new Edge(src, dest, wt);
+            graph[src].add(e);
+        }
+    }
+
+    static class Info {
+        int v, cost, stops;
+
+        public Info(int v, int c, int s) {
+            this.v = v;
+            this.cost = c;
+            this.stops = s;
+        }
+    }
+
+    public static int cheapestFlight(int n, int flights[][], int src, int dest, int k) {
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge> graph[] = new ArrayList[n];
+        createGraph(flights, graph);
+
+        int dist[] = new int[n];
+        for(int i=0; i<n; i++) {
+            if(i != src) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+
+        Queue<Info> q = new LinkedList<>();
+        q.add(new Info(src, 0, 0));
+
+        while(!q.isEmpty()) {
+            Info curr = q.remove();
+            if(curr.stops > k) {
+                break;
+            }
+
+            for(int i=0; i<graph[curr.v].size(); i++) {
+                Edge e = graph[curr.v].get(i);
+                int u = e.src;
+                int v = e.dest;
+                int wt = e.wt;
+
+                if(dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v] && curr.stops <= k) {
+                    dist[v] = dist[u] + wt;
+                    q.add(new Info(v, dist[v], curr.stops + 1));
+                }
+            }
+        }
+
+        //dist[dest]
+        if(dist[dest] == Integer.MAX_VALUE) return -1;
+        else return dist[dest];
+
+    }
     public static void main(String[] args) {
 
         createGraph();
