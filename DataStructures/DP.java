885383c5e6a1d2d3b247d2b077b1b03ddaa0e9cf
Pratyush <oggycandle786@gmail.com>
2023-11-24 21:11:17 +0530

    DPTabulation

--- DataStructures/DP.java
+++ DataStructures/DP.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 public class DP {
 
     public static int fib(int n, int f[]) {
 
         //Memoization (Top Down Approach)
-        
+
         if (n == 0 || n == 1)
             return n;
 
@@ -16,10 +18,27 @@ public class DP {
 
     }
 
+    public static int fibTabulation(int n) {
+        int dp[] = new int[n];
+        Arrays.fill(dp, 0);
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+
+
+
     public static void question1() {
         int n = 5;
         int f[] = new int[n];
         System.out.println(fib(n, f));
+        System.out.println(fibTabulation(n));
     }
 
 
