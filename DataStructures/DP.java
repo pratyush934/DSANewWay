71190c9bd0bf5ef472b895e4b5c9faef71c07d24
Pratyush <oggycandle786@gmail.com>
2023-11-25 15:57:36 +0530

    Good

--- DataStructures/DP.java
+++ DataStructures/DP.java
@@ -205,10 +205,37 @@ public class DP {
     }
 
     public static void question4() {
-        int arr[] = {4, 2, 7, 1, 3};
+        int arr[] = { 4, 2, 7, 1, 3 };
         int sum = 10;
         System.out.println(targetSumSubset(arr, sum));
     }
+
+    public static int unboundedKnapsack(int val[], int wt[], int W) {
+        int n = val.length;
+        int dp[][] = new int[n + 1][W + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 0; // Intialization
+        }
+
+        for (int j = 0; j < W + 1; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = i; j < W + 1; j++) {
+                if (wt[i - 1] <= W) { //valid 
+                                            //include                         //exclude
+                    dp[i][j] = Math.max((val[i - 1] + dp[i][j - wt[i - 1]]), dp[i - 1][j]);
+                } else {        //invalid
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][W];
+    }
+
     public static void main(String[] args) {
 
     }
