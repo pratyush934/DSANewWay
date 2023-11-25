58b1272f6b830ab7fdfae5e9137f194139386b2a
Pratyush <oggycandle786@gmail.com>
2023-11-25 15:12:49 +0530

    Kuch bhi

--- DataStructures/DP.java
+++ DataStructures/DP.java
@@ -160,6 +160,7 @@ public class DP {
 
             }
         }
+        // try to print the dp array
         return dp[n][W];
     }
 
@@ -167,20 +168,47 @@ public class DP {
         int val[] = { 15, 14, 10, 45, 30 };
         int wt[] = { 2, 5, 1, 3, 4 };
         int W = 7;
-        System.out.println(knapsack(val, wt, W, val.length););
-        
+        System.out.println(knapsack(val, wt, W, val.length));
 
-        int dp[][] = new int[val.length+1][W+1];
-        for(int i=0; i<dp.length; i++) {
-            for(int j=0; j<dp[0].length; j++) {
+        int dp[][] = new int[val.length + 1][W + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
                 dp[i][j] = -1;
             }
         }
         System.out.println(knapsackMemoization(val, wt, W, val.length, dp));
-        
+
         System.out.println(knapsackTabulation(val, wt, W));
     }
 
+    public static boolean targetSumSubset(int arr[], int sum) {
+        int n = arr.length;
+        boolean dp[][] = new boolean[n + 1][sum + 1];
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = true;
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; i++) {
+                int v = arr[i - 1];
+                // include
+                if (v <= j && dp[i - 1][j - v] == true) {
+                    dp[i][j] = true;
+                }
+                // exclude
+                else if (dp[i - 1][j] == true) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+
+    public static void question4() {
+        int arr[] = {4, 2, 7, 1, 3};
+        int sum = 10;
+        System.out.println(targetSumSubset(arr, sum));
+    }
     public static void main(String[] args) {
 
     }
