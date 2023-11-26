95baaf4f7699adc76405b4965a4f4daeb09f9bd1
Pratyush <oggycandle786@gmail.com>
2023-11-26 23:28:22 +0530

    EditDistance

--- DataStructures/DP.java
+++ DataStructures/DP.java
@@ -451,7 +451,45 @@ public class DP {
         return dp[n][m];
     }
 
-    
+    public static int editDistance(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+
+        // str1 ko convert karna hai str2 me
+
+        int dp[][] = new int[n + 1][m + 1];
+
+        // intializing
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                }
+                if (j == 0) {
+                    dp[i][j] = i;
+                }
+            }
+        }
+
+        // bottom up
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) { // same character
+                    dp[i][j] = dp[i - 1][j - 1];
+
+                } else { // differenct character
+                    /*
+                     * int add = dp[i][j-1] + 1;
+                     * int del = dp[i-1][j-1] + 1;
+                     * int replace = dp[i-1][j] + 1;
+                     */
+                    dp[i][j] = Math.min(dp[i][j - 1], Math.min(dp[i - 1][j - 1], dp[i - 1][j])) + 1;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
     public static void main(String[] args) {
 
     }
