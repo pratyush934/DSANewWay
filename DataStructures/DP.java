05fe679ca3666a3364a897546908cbfcce64d649
Pratyush <oggycandle786@gmail.com>
2023-11-26 22:11:59 +0530

    Adding question

--- DataStructures/DP.java
+++ DataStructures/DP.java
@@ -373,6 +373,36 @@ public class DP {
         return dp[n][m];
     }
 
+    public static int longestCommonSubstring(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+        int ans = 0;
+
+        int dp[][] = new int[n + 1][m + 1];
+
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int i = 0; i < dp.length; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    ans = Math.max(ans, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        return ans;
+
+    }
+
     public static void main(String[] args) {
 
     }
