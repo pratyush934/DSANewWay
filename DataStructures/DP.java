12ba085da7cb8c881a0f0165406f99829aa2322c
Pratyush <oggycandle786@gmail.com>
2023-11-27 01:16:28 +0530

    Adding wildCard

--- DataStructures/DP.java
+++ DataStructures/DP.java
@@ -490,6 +490,46 @@ public class DP {
         return dp[n][m];
     }
 
+    public static boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+
+        boolean dp[][] = new boolean[n + 1][m + 1];
+
+        // intialize
+        dp[0][0] = true;
+
+        // patern = " "
+        for (int i = 1; i < n + 1; i++) {
+            dp[i][0] = false;
+        }
+        // s = " "
+        for (int j = 1; j < m + 1; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+
+        // bottom up code
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                // case1 --> ith char == jth char YAA jth char of p is ?
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                // case2 --> jth char of p is *
+                else if (p.charAt(j - 1) == '*') {
+                                //ignore kiya || include kiya
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[n][m];
+
+    }
+
     public static void main(String[] args) {
 
     }
