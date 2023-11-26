8326216554a0227f55a77a973d1396f46a7649ec
Pratyush <oggycandle786@gmail.com>
2023-11-26 21:48:09 +0530

    Adding Question

--- DataStructures/DP.java
+++ DataStructures/DP.java
@@ -331,7 +331,7 @@ public class DP {
         }
 
         if (str1.charAt(n - 1) == str2.charAt(m - 1)) {
-            return dp[n][m] = lcsMemoization(str1, str2, n - 1, m - 1);
+            return dp[n][m] = lcsMemoization(str1, str2, n - 1, m - 1) + 1;
         }
 
         else {
@@ -341,6 +341,36 @@ public class DP {
             return dp[n][m] = (int) Math.max(ans1, ans2);
         }
 
+    }
+
+    public static int lcsTab(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+
+        int dp[][] = new int[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1; // Because we are counting same character in longest subsequence
+                } else {
+                    int ans1 = dp[i - 1][j];
+                    int ans2 = dp[i][j - 1];
+
+                    dp[i][j] = (int) Math.max(ans1, ans2);
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 
     public static void main(String[] args) {
