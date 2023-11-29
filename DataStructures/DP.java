1af153c2d70fd364c086622c6c3fc3b22b853066
Pratyush <oggycandle786@gmail.com>
2023-11-30 02:48:47 +0530

    Adding

--- DataStructures/DP.java
+++ DataStructures/DP.java
@@ -692,6 +692,31 @@ public class DP {
         return dp[i][j] = ans;
     }
 
+    public static int mamTab(int arr[]) {
+        int n = arr.length;
+        int dp[][] = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            if (i == j)
+                dp[i][j] = 0;
+        }
+
+        // bottom-up
+        for (int len = 2; len <= n - 1; len++) {
+            for (int i = 1; i <= n - len; i++) {
+                int j = i + len - 1;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k <= j - 1; k++) {
+                    int cost1 = dp[i][k];
+                    int cost2 = dp[k - 1][j];
+                    int cost3 = arr[i - 1] * arr[k] * arr[j];
+                    dp[i][j] = Math.min(dp[i][j], cost1 + cost2 + cost3);
+                }
+            }
+        }
+        return dp[1][n - 1];
+    }
+
     public static void question5() {
         int arr[] = { 1, 2, 3, 4, 5 };
         int n = arr.length;
