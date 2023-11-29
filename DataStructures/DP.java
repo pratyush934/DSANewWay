537537eec786d4b3d4daacd167a5ea052df876bd
Pratyush <oggycandle786@gmail.com>
2023-11-29 23:15:19 +0530

    Adding Catlan Tabulation

--- DataStructures/DP.java
+++ DataStructures/DP.java
@@ -555,10 +555,24 @@ public class DP {
         int ans = 0;
 
         for (int i = 0; i < n; i++)
-            ans += catlanMemoization(i, dp) + catlanMemoization(n - i - 1, dp);
+            ans += catlanMemoization(i, dp) * catlanMemoization(n - i - 1, dp);
 
         return dp[n] = ans;
 
+    }
+
+    public static int catlanTabulation(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < n; i++) { // Ci
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1]; // Ci = Cj + Cj-i-1;
+            }
+        }
+
+        return dp[n];
     }
 
     public static void main(String[] args) {
