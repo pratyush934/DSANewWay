c4c00edd181091aafeecf5df9cc88898a54b0066
Pratyush <oggycandle786@gmail.com>
2023-11-30 02:05:31 +0530

    Adding

--- DataStructures/DP.java
+++ DataStructures/DP.java
@@ -671,6 +671,36 @@ public class DP {
         return ans;
     }
 
+    public static int mcmMemoization(int arr[], int i, int j, int dp[][])/* arr.length -> n */ {
+
+        if (i == j)
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int ans = Integer.MAX_VALUE;
+        for (int k = i; k <= j - 1; k++) {
+            int cost1 = mcmMemoization(arr, i, k, dp);
+            int cost2 = mcmMemoization(arr, k + 1, j, dp);
+            int cost3 = arr[i - 1] * arr[k] * arr[j];
+
+            int finalCost = cost1 + cost2 + cost3;
+
+            ans = Math.min(ans, finalCost);
+        }
+        return dp[i][j] = ans;
+    }
+
+    public static void question5() {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int n = arr.length;
+        int dp[][] = new int[n][n];
+        for (int s = 0; s < n; s++) {
+            Arrays.fill(dp[s], -1);
+        }
+    }
+
     public static void main(String[] args) {
 
     }
