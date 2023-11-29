f1f86d8a24a4674250d09e211201173fd8025588
Pratyush <oggycandle786@gmail.com>
2023-11-30 01:39:31 +0530

    Imp

--- DataStructures/DP.java
+++ DataStructures/DP.java
@@ -651,6 +651,26 @@ public class DP {
         return dp[n];
     }
 
+    public static int mcm(int arr[], int i, int j) {
+        if (i == j)
+            return 0;
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int k = i; k < j - 1; k++) {
+            int cost1 = mcm(arr, i, k); // Ai...Ak ==> arr[i-1] * arr[k]
+            int cost2 = mcm(arr, k + 1, j); // Ai+1...Aj ==> arr[k] * arr[j]
+
+            int cost3 = arr[i - 1] * arr[k] * arr[j];
+
+            int finalCost = cost1 + cost2 + cost3;
+
+            ans = Math.min(ans, finalCost);
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
 
     }
