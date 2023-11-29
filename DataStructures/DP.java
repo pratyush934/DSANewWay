7247102e6b2cd044c0fdf1e834bbc4cd67b9cebf
Pratyush <oggycandle786@gmail.com>
2023-11-29 22:59:07 +0530

    Adding catlan memoization

--- DataStructures/DP.java
+++ DataStructures/DP.java
@@ -543,6 +543,24 @@ public class DP {
         return ans;
     }
 
+    public static int catlanMemoization(int n, int dp[]) {
+        Arrays.fill(dp, -1);
+
+        if (n == 0 || n == 1)
+            return 1;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++)
+            ans += catlanMemoization(i, dp) + catlanMemoization(n - i - 1, dp);
+
+        return dp[n] = ans;
+
+    }
+
     public static void main(String[] args) {
 
     }
