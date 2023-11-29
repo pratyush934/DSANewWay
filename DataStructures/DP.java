6d6951df1e28aabe48ac64ed4abeed0f0dd7cc93
Pratyush <oggycandle786@gmail.com>
2023-11-30 00:11:55 +0530

    Good

--- DataStructures/DP.java
+++ DataStructures/DP.java
@@ -593,7 +593,26 @@ public class DP {
         return dp[n] = ans;
     }
 
-    
+    public static int countBSTTabulation(int n) {
+        // count BST follows catlan patterns
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                // total number += leftNodes arrangement + RightNodes arrangement;
+                /*
+                 * int left = dp[j];
+                 * int right = dp[i-j-1];
+                 * dp[i] = left + right;
+                 */
+                dp[i] += dp[j] + dp[i - j - 1];
+            }
+        }
+
+        return dp[n];
+    }
 
     public static void main(String[] args) {
 
