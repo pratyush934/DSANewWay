39cd132675814b6b3812c5c06fbe8a97abe688c1
Pratyush <oggycandle786@gmail.com>
2023-11-30 00:34:14 +0530

    Adding Question

--- DataStructures/DP.java
+++ DataStructures/DP.java
@@ -614,6 +614,43 @@ public class DP {
         return dp[n];
     }
 
+    public static int mountainRanges(int n, int dp[]) {
+        Arrays.fill(dp, -1);
+
+        if (n == 0 || n == 1)
+            return 1;
+
+        if (dp[n] != -1)
+            return dp[n];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += mountainRanges(i, dp) * mountainRanges(n - i - 1, dp);
+        }
+
+        return dp[n] = ans;
+
+    }
+
+    public static int mountainRangesTab(int n) {
+        int dp[] = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                /*
+                 * int leftMountains = dp[j];
+                 * int rightMountains = dp[i-j-1];
+                 * total mountains = leftMountains + rightMountains
+                 */
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
 
     }
