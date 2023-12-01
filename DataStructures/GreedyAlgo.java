a8908bce2cc392e94628e26e3f1f008587378a49
Pratyush <oggycandle786@gmail.com>
2023-12-01 23:52:30 +0530

    Adding max length chain of pairs

--- DataStructures/GreedyAlgo.java
+++ DataStructures/GreedyAlgo.java
@@ -104,6 +104,28 @@ public class GreedyAlgo {
         System.out.println("Minimum absoulute diff " + minAbsDiff);
     }
 
+    public static void question4() {
+        /*
+         * Max length chain of pairs
+         */
+
+        int pairs[][] = { { 5, 24 }, { 39, 60 }, { 5, 28 }, { 27, 40 }, { 50, 90 } };
+
+        Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
+
+        int chainLength = 1;
+        int chainEnd = pairs[0][1]; // Last selected pair end //chain end
+
+        for (int i = 1; i < pairs.length; i++) {
+            if (pairs[i][0] > chainEnd) {
+                chainLength++;
+                chainEnd = pairs[i][1];
+            }
+        }
+        System.out.println("Maximum chain length is " + chainLength);
+    }
+
+    
     public static void main(String[] args) {
 
     }
