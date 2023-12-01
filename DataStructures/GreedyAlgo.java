707423b669257762f990c57dd7f03a49e46959a2
Pratyush <oggycandle786@gmail.com>
2023-12-01 21:43:15 +0530

    2D array ki sorting

--- DataStructures/GreedyAlgo.java
+++ DataStructures/GreedyAlgo.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class GreedyAlgo {
 
@@ -10,20 +12,33 @@ public class GreedyAlgo {
         int start[] = { 1, 3, 0, 5, 8, 5 };
         int end[] = { 2, 4, 6, 7, 9, 9 };
 
+        /* sorting porcess in the case if end time is not sorted */
+        int activities[][] = new int[start.length][3];
+        for (int i = 0; i < start.length; i++) {
+            activities[i][0] = i;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+
+        // lambda function --> shortform
+        // Yaha pe hum compiler ko bata rahe hai ki sorting kon se basis par karni hai.
+        // Yaha pr sorting matrix ke column 2 ke basis pr hoti hai
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+
         // end time basisi sorted
         int maxAct = 0;
         ArrayList<Integer> ans = new ArrayList<>();
 
         // 1st activity
         maxAct = 1;
-        ans.add(0);
-        int lastEnd = end[0];
+        ans.add(activities[0][0]);
+        int lastEnd = activities[0][2];
         for (int i = 1; i < end.length; i++) {
-            if (start[i] >= lastEnd) {
+            if (activities[i][1] >= lastEnd) {
                 // activity select
                 maxAct++;
-                ans.add(i);
-                lastEnd = end[i];
+                ans.add(activities[i][0]);
+                lastEnd = activities[i][2];
             }
         }
 
