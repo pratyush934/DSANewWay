27c8b2d6951510b99e0a7e9317e1a8b4229d5418
Pratyush <oggycandle786@gmail.com>
2023-12-01 23:35:41 +0530

    Adding another question

--- DataStructures/GreedyAlgo.java
+++ DataStructures/GreedyAlgo.java
@@ -87,6 +87,23 @@ public class GreedyAlgo {
         System.out.println("Final value is : " + finalValue);
     }
 
+    public static void question3() {
+        /* minmimum absolute difference */
+        int A[] = { 1, 2, 3 };
+        int B[] = { 2, 1, 3 };
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int minAbsDiff = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            minAbsDiff += Math.abs(A[i] - B[i]);
+        }
+
+        System.out.println("Minimum absoulute diff " + minAbsDiff);
+    }
+
     public static void main(String[] args) {
 
     }
