650ba627376202df2cf4871219616ea88ce4bc3f
Pratyush <oggycandle786@gmail.com>
2023-12-05 01:28:08 +0530

    Adding good thing

--- DataStructures/Heaps.java
+++ DataStructures/Heaps.java
@@ -73,12 +73,77 @@ public class Heaps {
                 int temp = arr.get(x);
                 arr.set(x, arr.get(par));
                 arr.set(par, temp);
+
+                // update
+                x = par;
+                par = (x - 1) / 2;
             }
             /* if a tree has n nodes then log(n) level is possible */
+        }
+
+        public int peek() {
+            // ArrayList ke 0th index me jo store hai wo subse min hoga
+            return arr.get(0);
+        }
+
+        public int remove() {
+            int data = arr.get(0);
+
+            // step1 - swap first & last
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size() - 1));
+            arr.set((arr.size() - 1), temp);
+
+            // step2 --> delete last
+            arr.remove(arr.size() - 1);
+
+            // step3 --> heapify
+            heapify(0);
+            return data;
+        }
+
+        private void heapify(int i) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int minIdx = i;
+
+            if (left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+                minIdx = left;
+            }
+
+            if (right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+                minIdx = right;
+            }
+
+            if (minIdx != i) {
+                // swap
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+
+                heapify(minIdx);
+            }
+        }
+
+        public boolean isEmpty() {
+            return arr.size() == 0;
+        }
+    }
+
+    public static void implementingHeaps() {
+        Heap h = new Heap();
+        h.add(3);
+        h.add(4);
+        h.add(1);
+        h.add(5);
+
+        while (!h.isEmpty()) {
+            System.out.println(h.peek() + "-->");
+            h.remove();
         }
     }
 
     public static void main(String[] args) {
-        introductionTOComparable();
+        implementingHeaps();
     }
 }
