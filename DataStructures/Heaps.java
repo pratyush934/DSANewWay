f36fe4a199a18928e6814ece2e3ed1413775735b
Pratyush <oggycandle786@gmail.com>
2023-12-04 23:37:41 +0530

    Good

--- DataStructures/Heaps.java
+++ DataStructures/Heaps.java
@@ -1,9 +1,23 @@
 //For terminal Ctrl + `
 //Remeber it
 
+import java.util.PriorityQueue;
+
 public class Heaps {
+
+    public static void implementingPQ() {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(1);
+        pq.add(-1);
+        pq.add(3);
+        pq.add(2);
+
+        System.out.println(pq);
+    }
+
     
+
     public static void main(String[] args) {
-        
+        implementingPQ();
     }
 }
