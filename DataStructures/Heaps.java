be4168f083948b37ed7667975412d61482bbbfea
Pratyush <oggycandle786@gmail.com>
2023-12-05 02:25:12 +0530

    Heap sort

--- DataStructures/Heaps.java
+++ DataStructures/Heaps.java
@@ -68,7 +68,7 @@ public class Heaps {
             int par = (x - 1) / 2; // par index
 
             // will swap until child is less than parent
-            while (arr.get(x) < arr.get(par)) { // O(log(n))
+            while (arr.get(x) < arr.get(par)) { // O(log(n)) //>
                 // swap
                 int temp = arr.get(x);
                 arr.set(x, arr.get(par));
@@ -107,11 +107,11 @@ public class Heaps {
             int right = 2 * i + 2;
             int minIdx = i;
 
-            if (left < arr.size() && arr.get(minIdx) > arr.get(left)) {
-                minIdx = left;
+            if (left < arr.size() && arr.get(minIdx) > arr.get(left)) { //>
+                minIdx = left; 
             }
 
-            if (right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+            if (right < arr.size() && arr.get(minIdx) > arr.get(right)) { //<
                 minIdx = right;
             }
 
@@ -141,6 +141,47 @@ public class Heaps {
             System.out.println(h.peek() + "-->");
             h.remove();
         }
+    }
+
+
+    public static void heapSort(int arr[]) {
+        //step1 -> build maxHeap
+        int n = arr.length;
+        for(int i=n/2; i>=0; i--) {
+            heapify(arr, i, n);
+        }
+        //step2 -> push largest at end
+        for(int i=n-1; i>0; i--) {
+            //swap
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, 0, i);
+        }
+    }
+    
+    private static void heapify(int arr[], int i, int size) {
+        int left = 2*i+1;
+        int right = 2*i+2;
+        int maxIdx = i;
+
+        if(left < size && arr[left] > arr[maxIdx]) {
+            maxIdx = left;
+        }
+
+        if(right < size && arr[right] < arr[maxIdx]) {
+            maxIdx = right;
+        }
+
+        if(maxIdx != i) {
+            int temp = arr[i];
+            arr[i] = arr[maxIdx];
+            arr[maxIdx] = temp;
+
+            heapify(arr, maxIdx, size);
+        }
+
     }
 
     public static void main(String[] args) {
