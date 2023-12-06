56902b4ab24eaab9ffc38eb54d10e6e7341d8b83
Pratyush <oggycandle786@gmail.com>
2023-12-06 20:34:19 +0530

    Inserting in trie

--- DataStructures/Trie.java
+++ DataStructures/Trie.java
@@ -1,6 +1,41 @@
 public class Trie {
+
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for (int level = 0; level < word.length(); i++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+
+        curr.eow = true;
+    }
+
+    public static void insertingElement() {
+        String words[] = { "the", "a", "there", "their", "any", "thee" };
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
+    }
+
     
+
     public static void main(String[] args) {
-        
+
     }
 }
