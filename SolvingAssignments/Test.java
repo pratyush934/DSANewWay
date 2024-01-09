8bb0679163f1fe6c312b3536f473333694e30eda
Pratyush <oggycandle786@gmail.com>
2024-01-09 23:47:54 +0530

    I

---
+++ SolvingAssignments/Test.java
@@ -0,0 +1,11 @@
+package SolvingAssignments;
+
+import java.util.Scanner;
+
+public class Test {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    char c = sc.next().charAt(0);
+    
+  }
+}
