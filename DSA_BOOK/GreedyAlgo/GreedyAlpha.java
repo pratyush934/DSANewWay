87e47a044e711e9d7b1bda1f6857a4fabeebc3f7
Pratyush <oggycandle786@gmail.com>
2024-02-27 00:13:22 +0530

    Completing 1 question

--- DSA_BOOK/GreedyAlgo/GreedyAlpha.java
+++ DSA_BOOK/GreedyAlgo/GreedyAlpha.java
@@ -1,5 +1,7 @@
 package DSA_BOOK.GreedyAlgo;
 
+import java.util.ArrayList;
+
 /**
  * Notes
  * 
@@ -11,12 +13,13 @@ public class GreedyAlpha {
     /**
      * Activity Selection
      * Max meetings in room
-     * Disjoint set 
+     * Disjoint set
      */
 
     /**
      * Aapko kaise pata chal raha hai ki greedy ka question hai.
-     * ki har ek step aap ye soch rahe ho ki kaise main jaldi kaam khatam karu taaki mereko naya kaam mile
+     * ki har ek step aap ye soch rahe ho ki kaise main jaldi kaam khatam karu taaki
+     * mereko naya kaam mile
      */
 
     /**
@@ -24,14 +27,36 @@ public class GreedyAlpha {
      * kaise hoga chaliye dekhte hai.
      */
 
-     /**
-      * 1. end time ko base bana kar sort kare
-      * 2. non-overlapping (disjoint) -> starttime >= last chosen activity
-      */
-      
+    /**
+     * 1. end time ko base bana kar sort kare (kyuki jaldi khtm hoga to jyada kaam
+     * karnege)
+     * 2. end time jiska kam ho uske select kare
+     * 2. non-overlapping (disjoint) -> starttime >= last chosen activity
+     * 4. count++;
+     */
+
+    int start[] = { 1, 3, 0, 5, 8, 5 };
+    int end[] = { 2, 4, 6, 7, 9, 9 };
+
+    int maxAct = 0;
+    ArrayList<Integer> list = new ArrayList<>();
+
+    maxAct = 1;
+    list.add(0);
+    int lastEnd = end[0];
+    for (int i = 1; i < end.length; i++) {
+      if (start[i] >= lastEnd) {
+        maxAct++;
+        list.add(i);
+        lastEnd = end[i];
+      }
+    }
+
+    System.out.println(maxAct);
+
   }
-  
+
   public static void main(String[] args) {
-    
+
   }
 }
