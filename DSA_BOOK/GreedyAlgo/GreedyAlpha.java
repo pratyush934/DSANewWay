7dabc488026718f674097333deb59c8f3f5c3e54
Pratyush <oggycandle786@gmail.com>
2024-02-26 23:41:11 +0530

    Starting Greedy Algo

---
+++ DSA_BOOK/GreedyAlgo/GreedyAlpha.java
@@ -0,0 +1,37 @@
+package DSA_BOOK.GreedyAlgo;
+
+/**
+ * Notes
+ * 
+ */
+
+public class GreedyAlpha {
+
+  public static void activitySelection() {
+    /**
+     * Activity Selection
+     * Max meetings in room
+     * Disjoint set 
+     */
+
+    /**
+     * Aapko kaise pata chal raha hai ki greedy ka question hai.
+     * ki har ek step aap ye soch rahe ho ki kaise main jaldi kaam khatam karu taaki mereko naya kaam mile
+     */
+
+    /**
+     * to isko end time ke hisaab se sort kar de.
+     * kaise hoga chaliye dekhte hai.
+     */
+
+     /**
+      * 1. end time ko base bana kar sort kare
+      * 2. non-overlapping (disjoint) -> starttime >= last chosen activity
+      */
+      
+  }
+  
+  public static void main(String[] args) {
+    
+  }
+}
