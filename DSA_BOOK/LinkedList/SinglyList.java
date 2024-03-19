e52557eafa1aa833b293fd9b52f718a4ed4639f0
Pratyush <oggycandle786@gmail.com>
2024-03-20 02:03:40 +0530

    Adding ll

--- DSA_BOOK/LinkedList/SinglyList.java
+++ DSA_BOOK/LinkedList/SinglyList.java
@@ -4,21 +4,14 @@ public class SinglyList {
 
   public static ListNode head;
   public static ListNode tail;
-  public static int size;
+  public static int size = 0;
 
-  public SinglyList() {
-    head = null;
-    tail = null;
-    size = 0;
-  }
-
-  public synchronized void addFirst(int data) {
-
+  public synchronized void ListNode(int data) {
     ListNode newNode = new ListNode(data);
     size++;
 
     if (head == null) {
-      head = newNode;
+      head = tail = newNode;
       return;
     }
 
@@ -27,125 +20,35 @@ public class SinglyList {
   }
 
   public synchronized void addLast(int data) {
-
     ListNode newNode = new ListNode(data);
     size++;
 
-    ListNode temp = head;
-    while (temp.next != null) {
-      temp = temp.next;
+    if (head == null) {
+      head = tail = newNode;
+      return;
     }
 
-    temp.next = newNode;
+    tail.next = newNode;
     tail = newNode;
   }
 
-  public synchronized void add(int data, int n) {
-
-    if (n == 0) {
-      addFirst(data);
-      return;
-    }
-
-    else if (n == size - 1) {
-      addLast(data);
-      return;
-    }
-
-    ListNode newNode = new ListNode(data);
-    size++;
-
-    int i = 0;
-    ListNode temp = head;
-    while (i < n - 1) {
-      temp = temp.next;
-      i++;
-    }
-
-    newNode.next = temp.next;
-    temp.next = newNode;
-
-  }
-
-  public synchronized void printLinkedList(ListNode head) {
-
+  public synchronized void print() {
     ListNode temp = head;
     while (temp != null) {
-      System.out.print(temp.data + " ");
+      System.out.print(temp.data + "-->");
       temp = temp.next;
     }
-    System.out.println("**");
+    System.out.println("---");
+
+    /* 
+     * for(; temp != null; temp = temp.next) {
+     *  sout(temp.data)
+     * }
+     */
 
   }
 
-  public synchronized int removeFirst(ListNode head) {
+  public static void main(String[] args) {
 
-    if (head == null) {
-      System.out.println("Sorry the LinkedList is Empty");
-      return -1;
-    }
-
-    if (size == 1) {
-      int val = head.data;
-      head = null;
-      return val;
-    }
-
-    else {
-      int val = head.data;
-      head = head.next;
-      return val;
-    }
   }
-
-  public synchronized int removeLast(ListNode head) {
-    if (head == null) {
-      System.out.println("Sorry the LinkedList is Empty");
-      return -1;
-    }
-
-    if (size == 1) {
-      int val = head.data;
-      head = null;
-      return val;
-    }
-
-    else {
-      ListNode temp = head;
-      while (temp.next.next != null) {
-        temp = temp.next;
-      }
-
-      int val = temp.next.data;
-      temp.next = null;
-
-      return val;
-    }
-  }
-
-  public synchronized int removeNth(ListNode head, int pos) {
-    if (pos == 0) {
-      return removeFirst(head);
-    } else if (pos == size - 1) {
-      return removeLast(head);
-    } else {
-      ListNode prev = head;
-      ListNode temp = head;
-      int i = 0;
-      while (i < pos) {
-        prev = temp;
-        temp = temp.next;
-        i++;
-      }
-      int val = temp.data;
-      prev.next = temp.next;
-      temp.next = null;
-      return val;
-    }
-  }
-
-  public synchronized void removeMathched(ListNode node) {
-    
-  }
-
 }
