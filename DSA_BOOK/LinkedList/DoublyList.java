1cead4a1daca6dfe1da1caad500b990a61ef8629
Pratyush <oggycandle786@gmail.com>
2024-03-26 00:11:04 +0530

    Adding complete DLL

--- DSA_BOOK/LinkedList/DoublyList.java
+++ DSA_BOOK/LinkedList/DoublyList.java
@@ -58,7 +58,84 @@ public class DoublyList {
     }
 
     public synchronized int removeFirst() {
+        if(head == null) {
+            System.out.println("Good Going but it is not practically possible");
+            return -1;
+        }
+
+        if(head == tail) {
+            int val = head.data;
+            head = tail = null;
+            return val;
+        }
+
+        int val = head.data;
+        head.next.prev = null;
+        head = head.next;
+        SIZE--;
+
+        return val;
+    }
+
+    public synchronized int removeLast() {
+        if(head == null) {
+            System.out.println("SOrry , we have failed you");
+            return -1;
+        }
+
+        if(head == tail) {
+            int val = head.data;
+            head = tail = null;
+            return val;
+        }
+
+        DLLNode previous = head;
+        while(previous.next.next != null)  {
+            previous = previous.next;
+        }
+        int val = previous.next.data;
+        previous.next.prev = null;
+        previous.next = null;
+        tail = previous;
+        SIZE--;
+
+        return val;
+    }
+
+    public synchronized int remove(int position) {
+        if(position == 0) {
+            return removeFirst();
+        } else if(position == SIZE-1) {
+            return removeLast();
+        }
+
+        DLLNode previous = head;
+        int i = 0;
+        while(i < position-1) {
+            previous = previous.next;
+            i++;
+        }
+
+        int val = previous.next.data;
+        previous.next.next.prev = previous;
+        previous.next = previous.next.next;
+        SIZE--;
+        return val;
+
+    }
+
+    public String toString() {
         
+        StringBuffer sb = new StringBuffer();
+
+        DLLNode temp = head;
+        while(temp != null) {
+            sb.append(temp.data+",");
+            temp = temp.next;
+        }
+
+        
+        return "[ " + sb.toString() + " ]";
     }
 
     public synchronized void print() {
@@ -77,6 +154,9 @@ public class DoublyList {
         dll.addLast(3);
         dll.add(5, 1);
         dll.print();
+        dll.remove(1);
+        dll.print();
+        System.out.println(dll.toString());
     }
 
 
