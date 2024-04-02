085dd75c9420ee74cb5112a1f74c888efef69371
Pratyush <oggycandle786@gmail.com>
2024-04-02 11:46:45 +0530

    Adding another question

--- SolvingAssignments/JPMogran.java
+++ SolvingAssignments/JPMogran.java
@@ -61,9 +61,36 @@ public class JPMogran {
 
             temp = temp.next;
         }
-
         return ans;
     }
+
+    public static String question3(String str) {
+        /* 
+         * A string is provided as an input from console, which consist of integer value
+         * Insert '*' if subseqent numbers are even and insert '-' if subsequent numbers are odd
+         */
+        /* 
+         * 21462675756  -> 214*6*2*67-5-7-56
+         */
+
+        StringBuffer sb = new StringBuffer();
+        for(int i=0; i<str.length()-1; i++) {
+            char ch1 = str.charAt(i);
+            char ch2 = str.charAt(i+1);
+            if((ch1-'0') % 2 == 0 && (ch2 - '0') % 2 == 0 ) {
+                sb.append(ch1);
+                sb.append('*');
+            } else if((ch1-'0') % 2 != 0 && (ch2 - '0') % 2 != 0) {
+                sb.append(ch1);
+                sb.append('-');
+            } else {
+                sb.append(ch1);
+            }
+        }
+        return sb.toString();
+    }
+
+
 
     public static void main(String[] args) {
         // String[] arr = {
@@ -73,12 +100,16 @@ public class JPMogran {
 
         // System.out.println(question1(arr, "princi", 6));
 
-        ListNode head = new ListNode(1);
+        /* ListNode head = new ListNode(1);
         head.next = new ListNode(0);
         head.next.next = new ListNode(1);
         head.next.next.next = new ListNode(1);
         head.next.next.next.next = new ListNode(0);
         
-        System.out.println(question2(head));
+        System.out.println(question2(head)); */
+
+        System.out.println(question3("21462675756"));
     }
+
+
 }
