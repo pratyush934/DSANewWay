b8b8996bd6801a4b83971e07e07ad2a8db65bb90
Pratyush <oggycandle786@gmail.com>
2024-05-23 01:33:20 +0530

    Adding

--- DataStructures/SlidingWindow.java
+++ DataStructures/SlidingWindow.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class SlidingWindow {
 
@@ -121,10 +122,125 @@ public class SlidingWindow {
         return maxLength;
     }
 
+    public static int fruitsInBasket(int arr[], int fruits) {
+
+        int left = 0, right = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int maxLength = 0;
+
+        while (right < arr.length) {
+            map.put(arr[right], map.getOrDefault(arr[right], 0) + 1);
+
+            if (map.size() > fruits) {
+
+                while (map.size() > fruits) {
+                    /*
+                     * Ye jo maine likha hai wo sikhne ki jarurat hai tumhe O(N+N) : O(3)
+                     */
+                    if (map.get(arr[left]) == 1) {
+                        map.remove(arr[left]);
+                    } else {
+                        map.put(arr[left], map.get(arr[left]) - 1);
+                    }
+                    left++;
+                }
+
+            } else {
+                maxLength = Math.max(maxLength, right - left + 1);
+            }
+            right++;
+
+        }
+
+        return maxLength;
+    }
+
+    public static int fruitsInBasketPartII(int arr[], int fruits) {
+
+        /* Code nahi chal raha hai */
+
+        int maxLength = 0;
+        int left = 0, right = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        while (right < arr.length) {
+            map.put(arr[right], map.getOrDefault(arr[right], 0) + 1);
+
+            if (map.size() > fruits) {
+                if (map.get(arr[left]) == 1)
+                    map.remove(map.get(arr[left]));
+                else
+                    map.put(arr[left], map.get(arr[left]) - 1);
+
+                left++;
+            } else {
+                maxLength = Math.min(maxLength, right - left + 1);
+            }
+            right++;
+        }
+        return maxLength;
+    }
+
+    public static int kDistinctCharacters(String str, int k) {
+        int left = 0, right = 0, maxLength = 0;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        while (right < str.length()) { // O(N+N) -> O(3)
+            char ch = str.charAt(right);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            if (map.size() > k) {
+
+                while (map.size() > k) {
+
+                    if (map.get(str.charAt(left)) == 1) {
+                        map.remove(str.charAt(left));
+                    } else {
+                        map.put(str.charAt(left), map.get(str.charAt(left)) - 1);
+                    }
+
+                    left++;
+                }
+            } else {
+                maxLength = Math.max(maxLength, right - left + 1);
+            }
+
+            right++;
+        }
+
+        return maxLength;
+    }
+
+    public static int kDistinctCharactersPartII(String str, int k) {
+        int left = 0, right = 0, maxLength = 0;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        while (right < str.length()) {
+            char ch = str.charAt(right);
+
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            if (map.size() > k) {
+                if (map.get(str.charAt(left)) == 1)
+                    map.remove(str.charAt(left));
+                else
+                    map.put(str.charAt(left), map.get(str.charAt(left)) - 1);
+
+                left++;
+            } else {
+                maxLength = Math.max(maxLength, right - left + 1);
+            }
+            right++;
+        }
+        return maxLength;
+    }
+
     public static void main(String[] args) {
 
-        int arr[] = { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 };
-        System.out.println(maxContinous1Part3(arr, 2));
-
+        String str = "aaabbccd";
+        System.out.println(kDistinctCharactersPartII(str, 3));
     }
 }
